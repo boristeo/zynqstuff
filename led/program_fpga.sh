1123f32c7ff48b2d9c2a53b56b5b37f993361d16
Boris Teodorovich <boris@degirum.com>
2025-03-11 17:50:05 -0700

    Got tcl script working. Blink program build is broken :(

---
+++ led/program_fpga.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+/tools/Xilinx/Vivado/2024.2/bin/xsdb program_fpga.tcl
+
---
+++ led/program_fpga.tcl
@@ -0,0 +1,27 @@
+connect -url TCP:localhost:3121
+
+# select xc7z010, load bitfile
+targets 4
+
+#rst -srst
+
+fpga zybo_example.bit
+
+targets -set -nocase -filter {name =~ "arm*#0"}
+dow fsbl.elf
+
+con; after 3000; stop
+
+#targets 1
+source ps7_init.tcl
+ps7_init
+
+#set deviceTreeOffset 0x100000
+#dow -data  "system.dtb" ${deviceTreeOffset}
+
+#dow xgpio_low_level_example_1.elf
+dow blinky_working.elf
+con;
+
+
+
