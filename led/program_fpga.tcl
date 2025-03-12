0c3fbd0de4a6ed0505e6393058941af1b57c21a2
Boris Teodorovich <boris@degirum.com>
2025-03-11 19:08:16 -0700

    Moved fsbl to other folder in repo root

--- led/program_fpga.tcl
+++ led/program_fpga.tcl
@@ -4,11 +4,12 @@ connect -url TCP:localhost:3121
 targets 4
 
 #rst -srst
+#after 10000
 
 fpga zybo_example.bit
 
 targets -set -nocase -filter {name =~ "arm*#0"}
-dow fsbl.elf
+dow ../zynq_fsbl/fsbl.elf
 
 con; after 3000; stop
 
@@ -19,7 +20,7 @@ ps7_init
 #set deviceTreeOffset 0x100000
 #dow -data  "system.dtb" ${deviceTreeOffset}
 
-dow xgpio_low_level_example_1.elf
+dow program.elf
 con;
 
 
